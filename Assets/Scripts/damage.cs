0575f9e53a48f6301d6454a1354798c11772bc13
JWHenr2 <jwhenr2@gmail.com>
2025-02-07 17:09:34 -0500

    Added damage Script + Level-JustinH
    - Added damage Script to game
    - Also added Level for Justin H
    - Begin working on level design from videos

---
+++ Assets/Scripts/damage.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class damage : MonoBehaviour
+{
+
+    enum damageType { moving, stationary}
+    [SerializeField] damageType type;
+    [SerializeField] Rigidbody rb;
+
+    [SerializeField] int damageAmount;
+    [SerializeField] int speed;
+    [SerializeField] int destroyTime;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if(type == damageType.moving)
+        {
+            rb.linearVelocity = transform.forward * speed;
+            Destroy(gameObject, destroyTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IDamage dmg = other.GetComponent<IDamage>();
+
+        if(dmg != null )
+        {
+            dmg.takeDamage(damageAmount);
+        }
+
+        if(type == damageType.moving)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
