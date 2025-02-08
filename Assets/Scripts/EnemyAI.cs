88d4b1af09a6fea36c55d509a28f04a6c5197b51
Aston Martinez <avmartinez@student.fullsail.edu>
2025-02-08 17:08:19 -0600

    fixed script name and added lecture 2 code

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,44 +1,87 @@
 using System.Collections;
-using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour, IDamage
 {
     [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Animator anim;
+
     [SerializeField] int HP;
+    [SerializeField] int animTransSpeed;
+    [SerializeField] int faceTargetSpeed;
+
+    [SerializeField] GameObject bullet;
+    [SerializeField] Transform shootPos;
+    [SerializeField] float shootRate;
 
     Color colorOrig;
+
+    float shootTimer;
+
+    Vector3 playerDir;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         colorOrig = model.material.color;
+        gameManager.instance.updateGameGoal(1);
     }
 
     // Update is called once per frame
     void Update()
     {
+        float agentSpeed = agent.velocity.normalized.magnitude;
+        float animCurSpeed = anim.GetFloat("Speed");
 
+        playerDir = gameManager.instance.player.transform.position - transform.position;
+
+        anim.SetFloat("Speed", Mathf.MoveTowards(animCurSpeed, agentSpeed, Time.deltaTime * animTransSpeed));
+        shootTimer += Time.deltaTime;
+        agent.SetDestination(gameManager.instance.player.transform.position);
+
+        if (shootTimer >= shootRate)
+        {
+            shoot();
+        }
+
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            faceTarget();
+        }
+    }
+
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(playerDir);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
     }
 
     public void takeDamage(int amount)
     {
-        HP -= amount; // subtract damage amount from health
+        HP -= amount;
 
-        StartCoroutine(flashRed()); // begins the flash of red
+        StartCoroutine(flashRed());
 
-        if (HP <= 0) // check if the enemy is dead
+        if (HP <= 0)
         {
-            Destroy(gameObject); // get rid of dead enemy
-
+            Destroy(gameObject);
+            gameManager.instance.updateGameGoal(-1);
         }
     }
 
     IEnumerator flashRed()
     {
-        model.material.color = Color.red; // sets the color to red
-        yield return new WaitForSeconds(0.1f); // flashes for only a second
-        model.material.color = colorOrig; // returns to original color
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = colorOrig;
+    }
+
+    void shoot()
+    {
+        shootTimer = 0;
+
+        Instantiate(bullet, shootPos.position, transform.rotation);
     }
 }
-
