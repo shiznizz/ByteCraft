9220cb9c8ecc5f02a355bd24f900b11f8d93c86a
TheAngelofChaos <davidhulett8@gmail.com>
2025-02-12 22:39:35 -0800

    Revert "iPickUp RAHHHHHHH"
    This reverts commit 77cc9d6c27044229e86de07545c8c74b1c63dafe.

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -1,16 +1,89 @@
+using UnityEditor;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+
+public class gameManager : MonoBehaviour
 {
+    public static gameManager instance;
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+    [SerializeField] TMP_Text goalCountText;
+
+    public Image playerHPBar;
+    public Image JPFuelGauge;
+    public GameObject playerDamageScreen;
+    public bool isPaused;
+    public GameObject player;
+    public playerController playerScript;
+
+    int goalCount;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        instance = this;
+        player = GameObject.FindWithTag("Player");
+        playerScript = player.GetComponent<playerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if(menuActive == null)
+            {
+                statePause();
+                menuActive = menuPause;
+                menuActive.SetActive(true);
+            }
+            else if(menuActive == menuPause)
+            {
+                stateUnpause();
+            }
+        }
+    }
+
+    public void statePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void stateUnpause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(false);
+        menuActive = null;
+    }
+
+
+    public void updateGameGoal(int amount)
+    {
+        goalCount += amount;
+
+        if (goalCount <= 0)
+        {
+            statePause();
+            menuActive = menuWin;
+            menuActive.SetActive(true);
+        }
+    }
+
+    public void youLose()
+    {
+        statePause();
+        menuActive = menuLose;
+        menuActive.SetActive(true);
     }
 }
