14c14aac082681bba0f0ac15cef927c76304f4d5
shiznizz <jll40@live.com>
2025-02-26 15:40:17 -0600

    added selecting functions to the equipment slots.
    working on adding the unequip

--- Assets/Scripts/Inventory/equipSlot.cs
+++ Assets/Scripts/Inventory/equipSlot.cs
@@ -1,8 +1,10 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
+//using UnityEngine.UIElements;
 
-public class equipSlot : MonoBehaviour
+public class equipSlot : MonoBehaviour, IPointerClickHandler
 {
 
     [SerializeField] itemSO item;
@@ -10,8 +12,11 @@ public class equipSlot : MonoBehaviour
     [SerializeField] Image itemIcon;
 
     [SerializeField] GameObject equippedSlot;
+    [SerializeField] GameObject selected;
+
 
     public bool isFull;
+    public bool isSelected;
 
     private weaponStats gun;
 
@@ -38,5 +43,48 @@ public class equipSlot : MonoBehaviour
 
         inventoryManager.instance.removeItem(item);
 
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            //if (isSelected)
+            //    gameManager.instance.deselectItem();
+            //else if (!isSelected && isFull)
+            
+            if (isFull && !isSelected)
+            {
+                if (gameManager.instance.selectedEquipSlot != null)
+                {
+                    gameManager.instance.selectedEquipSlot.transform.GetChild(1).gameObject.SetActive(false);
+                    gameManager.instance.selectedEquipSlot.GetComponent<equipSlot>().isSelected = false;
+                }
+                Debug.Log("1");
+                selectItem(eventData);
+            }
+            else if (isSelected)
+            {
+                selected.SetActive(false);
+                isSelected = false;
+            }
+
+
+        }
+    }
+
+    public void selectItem(PointerEventData eventData)
+    {
+        //gameManager.instance.deselectItem();
+        Debug.Log("2");
+        gameManager.instance.selectedEquipSlot = null;
+        gameManager.instance.selectedEquipSlot = eventData.pointerClick;
+        
+        selected.SetActive(true);
+        isSelected = true;
+        //displaySlot.SetActive(true);
+        //itemDescription.text = item.itemDescription;
+        //itemName.text = item.itemName;
+        //itemIcon.sprite = item.itemIcon;
     }
 }
