49a251362ca24e963c9ec7f6e992023242639442
Aston Martinez <avmartinez@student.fullsail.edu>
2025-03-16 16:23:07 -0500

    finished implementing AOE support enemy

--- Assets/Scripts/Enemy Related Scripts/EnemyAI.cs
+++ Assets/Scripts/Enemy Related Scripts/EnemyAI.cs
@@ -16,14 +16,14 @@ public class enemyAI : MonoBehaviour, IDamage, lootDrop
     [SerializeField] movementType movement;
     [SerializeField] GameObject target;
     [SerializeField] Renderer model;
-    [SerializeField] NavMeshAgent agent;
+    [SerializeField] public NavMeshAgent agent;
     [SerializeField] Animator anim;
     private GameObject originalTarget;
 
     [Header("Enemy Stats")]
     [SerializeField] Image hpFillBar;
     [SerializeField] Canvas hpBar;
-    [SerializeField] int HP;
+    [SerializeField] public int HP;
     [SerializeField] int animTransSpeed;
     [SerializeField] int faceTargetSpeed;
     [SerializeField] int FOV; //Field of View
@@ -526,5 +526,12 @@ public class enemyAI : MonoBehaviour, IDamage, lootDrop
         playerInDroneRange = state;
     }
 
+    #endregion
+
+    #region AOESupport
+    public void SetHP(int newHP)
+    {
+        this.HP = newHP;
+    }
     #endregion
 }
