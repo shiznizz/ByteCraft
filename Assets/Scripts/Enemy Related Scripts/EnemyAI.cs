0a84e2bab6d7e73cca154766d972fcb107b9aaea
Aston Martinez <avmartinez@student.fullsail.edu>
2025-03-24 20:31:06 -0500

    Added audio for melee grunt enemy

--- Assets/Scripts/Enemy Related Scripts/EnemyAI.cs
+++ Assets/Scripts/Enemy Related Scripts/EnemyAI.cs
@@ -22,7 +22,7 @@ public class enemyAI : MonoBehaviour, IDamage, lootDrop
     [SerializeField] Renderer model;
     [SerializeField] public NavMeshAgent agent;
     [SerializeField] Animator anim;
-    [SerializeField] private bool isDrone = false;
+    //[SerializeField] private bool isDrone = false;
     [SerializeField] GameObject hpBarTarget;
     private GameObject originalTarget;
     private bool isKami;
@@ -77,8 +77,11 @@ public class enemyAI : MonoBehaviour, IDamage, lootDrop
     private Renderer bodyRenderer;
 
     [Header("Audio")]
-    [SerializeField] AudioSource enemyAudio;
-    [SerializeField] AudioClip hurtSound;
+    //[SerializeField] AudioSource enemyAudio;
+    [SerializeField] ModulatedSoundBank enemyHurtSounds;
+    [SerializeField] ModulatedSoundBank enemyFootsteps;
+    [SerializeField] ModulatedSoundBank enemyAttackSounds;
+    [SerializeField] ModulatedSoundBank enemyDeathSounds;
 
     Vector3 startingPos;
     float roamTimer;
@@ -140,8 +143,8 @@ public class enemyAI : MonoBehaviour, IDamage, lootDrop
     // Update is called once per frame
     void Update()
     {
-        
         updateEnemyUI();
+
         // if stunned, nav mesh will stop and skip rest of AI's logic
         if (isStunned || godMode || isKami)
         {
@@ -152,7 +155,6 @@ public class enemyAI : MonoBehaviour, IDamage, lootDrop
         {
             agent.isStopped = false;
         }
-
         
         if (type != enemyType.stationary)
         {
@@ -190,6 +192,11 @@ public class enemyAI : MonoBehaviour, IDamage, lootDrop
             checkRoam();
         else if (!playerInRange)
             checkRoam();
+
+        if (agent.remainingDistance > 0.01f)
+        {
+            PlayMovementSound();
+        }
     }
 
     void updateEnemyUI()
@@ -356,7 +363,7 @@ public class enemyAI : MonoBehaviour, IDamage, lootDrop
     public void takeDamage(int amount)
     {
         if (isDead || godMode) return;
-        if (isDrone) enemyAudio.PlayOneShot(hurtSound);
+        StartCoroutine(PlayHurtSound());
 
         if (movement == movementType.seeking)
         {
@@ -370,7 +377,7 @@ public class enemyAI : MonoBehaviour, IDamage, lootDrop
             HP -= effectiveDamage;
 
             // debig log to confirm dmg is taken
-            Debug.Log("Enemy took: " + amount + " damage");
+            //Debug.Log("Enemy took: " + amount + " damage");
 
             // instantiate the floating damage text only once when damage is taken.
             if (floatingDamageTextPrefab != null)
@@ -463,10 +470,10 @@ public class enemyAI : MonoBehaviour, IDamage, lootDrop
 
     private void handleDeath()
     {
+        StartCoroutine(PlayDeathSound());
         hpBar.gameObject.SetActive(false);
-        Debug.Log("Hitting handle death.");
         AlarmDrone droneScript = GetComponent<AlarmDrone>();
-        if (droneScript != null) Debug.Log("Found drone script!");
+        //if (droneScript != null) Debug.Log("Found drone script!");
         /*if (isDrone)
         {
             Debug.Log("Hitting if statement.");
@@ -530,6 +537,7 @@ public class enemyAI : MonoBehaviour, IDamage, lootDrop
     void shoot()
     {
         shootTimer = 0;
+        StartCoroutine(PlayWeaponSound());
 
         if (anim != null)
             anim.SetTrigger("Shoot");
@@ -559,6 +567,7 @@ public class enemyAI : MonoBehaviour, IDamage, lootDrop
         {
             kamikazeAttack();
         }
+        StartCoroutine(PlayWeaponSound());
     }
 
     void kamikazeAttack()
@@ -674,6 +683,48 @@ public class enemyAI : MonoBehaviour, IDamage, lootDrop
     public void SetHP(int newHP)
     {
         this.HP = newHP;
+    }
+    #endregion
+
+    #region Audio
+    IEnumerator PlayHurtSound()
+    {
+        if (enemyHurtSounds != null)
+        {
+            float clipDuration = enemyHurtSounds.GetClipDuration();
+            enemyHurtSounds.PlayCurrentClip();
+            yield return new WaitForSeconds(clipDuration);
+        }
+    }
+
+    IEnumerator PlayMovementSound()
+    {
+        if (enemyFootsteps != null)
+        {
+            float clipDuration = enemyDeathSounds.GetClipDuration();
+            enemyDeathSounds.PlayCurrentClip();
+            yield return new WaitForSeconds(clipDuration);
+        }
+    }
+
+    IEnumerator PlayDeathSound()
+    {
+        if (enemyDeathSounds != null)
+        {
+            float clipDuration = enemyDeathSounds.GetClipDuration();
+            enemyDeathSounds.PlayCurrentClip();
+            yield return new WaitForSeconds(clipDuration);
+        }
+    }
+
+    IEnumerator PlayWeaponSound()
+    {
+        if (enemyAttackSounds != null)
+        {
+            float clipDuration = enemyAttackSounds.GetClipDuration();
+            enemyAttackSounds.PlayCurrentClip();
+            yield return new WaitForSeconds(clipDuration);
+        }
     }
     #endregion
 }
