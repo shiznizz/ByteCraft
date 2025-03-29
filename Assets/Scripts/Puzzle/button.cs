89a0334f34103c774098e4bc1983087c1546482a
Aston Martinez <avmartinez@student.fullsail.edu>
2025-03-29 18:54:48 -0500

    fixed issue with button hold bar getting stuck on screen

--- Assets/Scripts/Puzzle/button.cs
+++ Assets/Scripts/Puzzle/button.cs
@@ -59,6 +59,10 @@ public class buttons : MonoBehaviour
                 holdTime = 0;
             }
         }
+        else
+        {
+            holdBar.gameObject.SetActive(false);
+        }
     }
 
     public void ReleaseButton()
